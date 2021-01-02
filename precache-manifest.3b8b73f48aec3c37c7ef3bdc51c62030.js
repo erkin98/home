e890bb3c951d90f85074f07db347ccd9a039f4a9
Erkin <erkinqara@gmail.com>
2021-01-02 18:48:42 +0400

    Updates

---
+++ precache-manifest.3b8b73f48aec3c37c7ef3bdc51c62030.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529adf75ad2154e7edea20587b772e44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3855e390db1d70774cfa",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3855e390db1d70774cfa",
+    "url": "/static/js/main.23113910.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "814e6a0f5d05c2819f72a372e7937c9b",
+    "url": "/static/media/erkinqara.814e6a0f.jpg"
+  },
+  {
+    "revision": "189f1ff3a7da44a0fdc04bd099ef5334",
+    "url": "/static/media/resume.189f1ff3.pdf"
+  }
+]);
